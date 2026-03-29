65e6c45b656d8e568a9b9f4197b21c1a91f4d8ea
extrakrunchy <bp85p@protonmail.com>
2026-03-29 16:52:25 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,17 @@
+import "./globals.css";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Pick A Batch",
+  description: "Know Your Batch. Choose Better. Feel Better.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
